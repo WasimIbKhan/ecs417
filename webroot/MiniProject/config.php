09e124d4f7f1c3cd9cab6e49d648f098f1e84263
ANormalDude <61946126+WasimIbKhan@users.noreply.github.com>
2021-04-10 23:23:02 +0100

    Update config.php

--- webroot/MiniProject/config.php
+++ webroot/MiniProject/config.php
@@ -1,12 +1,13 @@
 <?php
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "ecs417";
-    // Creates connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Checks connection
-    if ($conn->connect_error) {
-     die("Connection failed: " . $conn->connect_error);
-    }
+	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	$dbport = getenv("MYSQL_SERVICE_PORT");
+	$dbuser = getenv("DATABASE_USER");
+	$dbpwd = getenv("DATABASE_PASSWORD");
+	$dbname = getenv("DATABASE_NAME");
+	// Creates connection
+	$conn = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+	// Checks connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
 ?>
