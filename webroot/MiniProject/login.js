d01a98b1b457ff548ebd66f22aed4932387867d6
ANormalDude <61946126+WasimIbKhan@users.noreply.github.com>
2021-04-12 15:44:44 +0100

    Create login.js

---
+++ webroot/MiniProject/login.js
@@ -0,0 +1,14 @@
+function validate() {
+  var emailInput = document.getElementById("email").value;
+  var passwordInput = document.getElementById("password").value;
+  if(emailInput === "") {
+    emailInput.style.backgroundColor = "pink";
+    return false;
+  } 
+  if(passwordInput === "") {
+    emailInput.style.backgroundColor = "pink";
+    return false;
+  }
+  
+  return true;
+}
